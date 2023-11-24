b83aa16c09dfa8ede3c62bcfe8d12af8c0621731
hp <manjeetyadav725@gmail.com>
2023-11-24 11:26:02 +0530

    First service

---
+++ src/main/java/com/firstproject/springbootfirstproject/service.java
@@ -0,0 +1,12 @@
+package com.firstproject.springbootfirstproject;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class service {
+
+    public String home(){
+        return "home";
+    }
+
+}
